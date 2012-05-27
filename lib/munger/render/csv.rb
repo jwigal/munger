d21cfc99c6698493a8b51e41f77b0b7f473cb5f6
Jeff Wigal <jeff@assignr.com>
2012-05-26 20:39:51 -0400

    use CSV or FasterCSV for parsing

--- lib/munger/render/csv.rb
+++ lib/munger/render/csv.rb
@@ -4,22 +4,29 @@ module Munger #:nodoc:
     
       attr_reader :report
       
+      def csv_class
+        if defined?(::CSV)
+          ::CSV
+        elsif defined?(::FasterCSV)
+          ::FasterCSV
+        else
+          raise NoMethodError, "Could not find a csv parser"
+        end
+      end
+      
       def initialize(report)
         @report = report
       end
       
       def render
-        output = []
-
-        # header
-        output << @report.columns.collect { |col| @report.column_title(col).to_s }.join(',')
-        
-        # body
-        @report.process_data.each do |row|
-          output << @report.columns.collect { |col| row[:data][col].to_s }.join(',')
-        end
-        
-        output.join("\n")
+        csv_class.generate do |output|
+          # header
+          output << @report.columns.collect { |col| @report.column_title(col).to_s }        
+          # body
+          @report.process_data.each do |row|
+            output << @report.columns.collect { |col| row[:data][col].to_s }
+          end
+        end        
       end
       
       def valid?
