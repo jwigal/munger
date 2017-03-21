4f4ea1cb545878432572c1e3060bd4e48621de35
Jeff Wigal <jeff@bluezebrasports.com>
2017-03-21 14:31:18 -0400

    prepend CSV export with UTF-8 Byte Order Mark (BOM) to play nicely with Excel

--- lib/munger/render/csv.rb
+++ lib/munger/render/csv.rb
@@ -1,9 +1,9 @@
 module Munger #:nodoc:
   module Render #:nodoc:
     class CSV #:nodoc:
-    
+
       attr_reader :report
-      
+
       def csv_class
         if defined?(::FasterCSV)
           ::FasterCSV
@@ -13,28 +13,30 @@ module Munger #:nodoc:
           raise NoMethodError, "Could not find a csv parser"
         end
       end
-      
+
       def initialize(report)
         @report = report
       end
-      
+
       def render
-        csv_class.generate do |output|
+        csv_data = csv_class.generate do |output|
           # header
-          output << @report.columns.collect { |col| @report.column_title(col).to_s }        
+          output << @report.columns.collect { |col| @report.column_title(col).to_s }
           # body
           @report.process_data.each do |row|
             if row[:meta][:data]
               output << @report.columns.collect { |col| row[:data][col].to_s }
             end
           end
-        end        
+        end
+        # https://donatstudios.com/CSV-An-Encoding-Nightmare
+        "\uFEFF" + csv_data
       end
-      
+
       def valid?
         @report.is_a? Munger::Report
       end
-    
+
     end
   end
 end
